673d4b0735aa46e4ed1eceacbec6656aca03b862
a-takahashi615 <asu61510ka@gmail.com>
2026-04-11 13:43:02 +0900

    kadai_005のアップロード

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,10 +1,12 @@
 // 変数の宣言
 let weight;
 let height;
+let bmi;
 
 // 変数に値を代入
-weight = '68';
-height = '1.7';
+weight = 68;
+height = 1.7;
+bmi = weight / (height * height);
 
 // コンソールへの出力
-console.log(weight / (height * height));
+console.log(bmi);
