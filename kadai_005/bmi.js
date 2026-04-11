c8e7ce248be4456c1399d825311c0afed4187694
a-takahashi615 <asu61510ka@gmail.com>
2026-04-11 13:37:47 +0900

    kadai_005のアップロード

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,10 @@
-// 体重・身長を記載
-const weight = 68;
-const height = 1.7;
+// 変数の宣言
+let weight;
+let height;
+
+// 変数に値を代入
+weight = '68';
+height = '1.7';
 
 // コンソールへの出力
 console.log(weight / (height * height));
